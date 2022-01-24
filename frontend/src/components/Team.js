ed70d9d3f0a2f0f6b202ade0ab5bce0182494f1b
dannyp0930 <dannyp0930@gmail.com>
2022-01-24 19:40:50 +0900

    FEAT: 팀 페이지 기본 구성

--- frontend/src/components/Team.js
+++ frontend/src/components/Team.js
@@ -1,3 +1,42 @@
-export const Team = () => {
-  return <div>Team page</div>;
-};
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
+export function Team() {
+  return (
+    <Grid container spacing={2} mt={2}>
+      <Grid item xs={4}>팀 목록</Grid>
+      <Grid item xs={8}>팀 명</Grid>
+      <Grid item xs={4}>
+        <Box
+          sx={{
+            width: '100%',
+            height: '50rem',
+            backgroundColor: 'primary.dark',
+          }}
+        >
+        </Box>
+      </Grid>
+      <Grid item xs={8}>
+        <Box
+          display='grid'
+          gap={2}
+          sx={{
+            width: '100%',
+            height: '50rem',
+            backgroundColor: 'primary.dark',
+          }}
+        >
+          
+          <Box
+            sx={{
+              width: '50%',
+              height: '10rem',
+              backgroundColor: 'primary.main',
+            }}
+          ></Box>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+}
