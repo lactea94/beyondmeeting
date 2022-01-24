f315e129bef60f1453eaf60c1bf019aad7c9df81
pjyc17 <pjyc17@gmail.com>
2022-01-24 23:07:51 +0900

    FEAT: MYPAGE

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,3 +1,21 @@
-export const Profile = () => {
-  return <div>Profile page</div>;
+import { useState } from "react"
+
+export function Profile() {
+  const [userImg, setUserImg] = useState(false) // false는 유저 이미지를 가져왔더니 이미지가 기본 상태
+  const username = 'park'
+  return (
+    <div className="profile-container">
+      <div>
+        { userImg ? (
+          <h1 className="profile-info">YOUR IMG IS HERE</h1>
+        ) : (
+          <div className="profRoundImg">
+            <img src="img/그림1.png" alt="우왕" />
+          </div>
+        )
+        }
+      </div>
+      <h1>user's name : {username}</h1>
+    </div>
+  )
 };
