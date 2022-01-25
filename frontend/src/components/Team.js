3c98a72536b7480710e05c720d93dd0e5041cf3e
dannyp0930 <dannyp0930@gmail.com>
2022-01-25 14:37:50 +0900

    STYLE: 스타일링 정리

--- frontend/src/components/Team.js
+++ frontend/src/components/Team.js
@@ -1,42 +1,113 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import { Card, CardContent } from '@mui/material';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 export function Team() {
-  return (
-    <Grid container spacing={2} mt={2}>
-      <Grid item xs={4}>팀 목록</Grid>
-      <Grid item xs={8}>팀 명</Grid>
-      <Grid item xs={4}>
-        <Box
-          sx={{
-            width: '100%',
-            height: '50rem',
-            backgroundColor: 'primary.dark',
-          }}
-        >
-        </Box>
-      </Grid>
+  const user = {
+    leader: true,
+    team: [
+      {id:0, name:'팀 1', leader: true, meeting: [1, 2,]},
+      {id:1, name:'팀 2', leader: false},
+      {id:2, name:'팀 3', leader: false}
+    ],
+    nowTeam: 0
+  }
+  let create
+  if (user.team[user.nowTeam].leader) {
+    create =
       <Grid item xs={8}>
         <Box
-          display='grid'
-          gap={2}
           sx={{
+            display: 'flex',
             width: '100%',
-            height: '50rem',
-            backgroundColor: 'primary.dark',
+            justifyContent: 'flex-end'
           }}
         >
-          
-          <Box
-            sx={{
-              width: '50%',
-              height: '10rem',
-              backgroundColor: 'primary.main',
-            }}
-          ></Box>
+          <Button item variant="contained" sx={{m:1}}>회의 생성</Button>
+          <Button item variant="contained" sx={{m:1}}>팀 관리</Button>
         </Box>
       </Grid>
+  }
+  return (
+    <Grid container>
+      <Grid item xs={4} container>
+        <Grid className='team-list-title' item>
+          <h1>팀 목록</h1>
+        </Grid>
+        <Grid
+          item
+          container
+          direction='column'
+          sx={{
+            width: '100%',
+            backgroundColor: '#fb8c00',
+            m: 1,
+          }}
+        >
+          <Grid item>
+            <Card sx={{m:1}}>
+              <CardContent>
+                1팀
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item>
+            <Card sx={{m:1}}>
+              <CardContent>
+                1팀
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={8}container>
+        <Grid className='meeting-list-title' item>
+          <h1>회의 목록</h1>
+        </Grid>
+        <Grid
+          item
+          container
+          sx={{
+            width: '100%',
+            backgroundColor: '#009688',
+            m: 1,
+          }}
+        >
+          <Grid item xs={4} sx={{p:'8'}}>
+            <Card sx={{m:1}}>
+              <CardContent>
+                회의 1
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={4} sx={{px:'1'}}>
+            <Card sx={{m:1}}>
+              <CardContent>
+                회의 2
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={4} sx={{px:'1'}}>
+            <Card sx={{m:1}}>
+              <CardContent>
+                회의 3
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={4} sx={{px:'1'}}>
+            <Card sx={{m:1}}>
+              <CardContent>
+                회의 4
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={4}>
+          <Button variant="contained" sx={{m:1}}>팀 생성</Button>
+        </Grid>
+      {create}
     </Grid>
   );
 }
