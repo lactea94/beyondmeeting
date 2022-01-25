4f48f3952562ac2dff2c6d526096d14266f6a0e8
유진주 <jinju6288@naver.com>
2022-01-25 12:52:49 +0900

    FEAT:Facebook,github로그인삭제

--- backend/src/main/java/com/beyondmeeting/backend/BackendApplication.java
+++ backend/src/main/java/com/beyondmeeting/backend/BackendApplication.java
@@ -1,9 +1,12 @@
 package com.beyondmeeting.backend;
 
+import com.beyondmeeting.backend.login.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class) //AppProperties 활성화 - 진주/0125
 public class BackendApplication {
 
 	public static void main(String[] args) {
