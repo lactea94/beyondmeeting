55ba586bdb0d36f56613ad1dd14f5ecdfdfedf3b
dannyp0930 <dannyp0930@gmail.com>
2022-01-26 11:25:56 +0900

    FEAT: 팀 생성 모달 추가

--- frontend/src/components/Team.js
+++ frontend/src/components/Team.js
@@ -1,42 +1,30 @@
+import * as React from 'react';
 import Grid from '@mui/material/Grid';
-import { Card, CardContent } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
 
 export function Team() {
   const user = {
     leader: true,
     team: [
-      {id:0, name:'팀 1', leader: true, meeting: ['a', 'b', 'c']},
+      {id:0, name:'팀 1', leader: true, meeting: ['a : 회의 완료', 'b : 회의 완료', 'c : 회의 중']},
       {id:1, name:'팀 2', leader: false, meeting: ['z', 's']},
       {id:2, name:'팀 3', leader: false, meeting: ['hi', 'hello', 'zbc', 'adf']}
     ],
     nowTeam: 0
   }
-  let CreateButton
-  if (user.team[user.nowTeam].leader) {
-    CreateButton =
-      <Grid item xs={8}>
-        <Box
-          sx={{
-            display: 'flex',
-            width: '100%',
-            justifyContent: 'flex-end'
-          }}
-        >
-          <Button item variant="contained" sx={{m:1}}>회의 생성</Button>
-          <Button item variant="contained" sx={{m:1}}>팀 관리</Button>
-        </Box>
-      </Grid>
-  }
 
+  
   function teamList() {
     const names = []
     for (let index = 0; index < user.team.length; index++) {
       names.push(user.team[index].name)
     }
     const nameList = names.map((name) =>
-      <Grid item>
+    <Grid item>
         <Card sx={{m:1}}>
           <CardContent>
             {name}
@@ -54,16 +42,16 @@ export function Team() {
         backgroundColor: '#fb8c00',
         m: 1,
       }}
-    >
+      >
       {nameList}
     </Grid>
     )
   }
-
+  
   function meetList() {
     const meets = user.team[user.nowTeam].meeting
     const nameList = meets.map((name) =>
-      <Grid item xs={4} sx={{p:'1'}}>
+    <Grid item xs={4} sx={{p:'1'}}>
         <Card sx={{m:1}}>
           <CardContent>
             {name}
@@ -85,7 +73,56 @@ export function Team() {
       </Grid>
     )
   }
+  
+  const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+  };
+  
+  const [createTeamOpen, setcreateTeamOpen] = React.useState(false);
+  
+  function createTeamButton() {
+    const handleCreateTeamOpen = () => setcreateTeamOpen(true);
+    const handleCreateTeamClose = () => setcreateTeamOpen(false);
+    return (
+      <Grid item xs={4}>
+        <Button onClick={handleCreateTeamOpen} variant="contained" sx={{m:1}}>팀 생성</Button>
+        <Modal
+          open={createTeamOpen}
+          onClose={handleCreateTeamClose}
+          >
+          <Card sx={modalStyle}>
+            팀 생성
+          </Card>
+        </Modal>
+      </Grid>
+    );
+  }
 
+  let manageTeamButtons
+  if (user.team[user.nowTeam].leader) {
+    manageTeamButtons =
+      <Grid item xs={8}>
+        <Box
+          sx={{
+            display: 'flex',
+            width: '100%',
+            justifyContent: 'flex-end'
+          }}
+        >
+          <Button item variant="contained" sx={{m:1}}>회의 생성</Button>
+          <Button item variant="contained" sx={{m:1}}>팀 관리</Button>
+        </Box>
+      </Grid>
+  }
+
+  // function 
+  
   return (
     <Grid container>
       <Grid className='team-list-title' item xs={4}>
@@ -100,10 +137,8 @@ export function Team() {
       <Grid item xs={8}container>
         {meetList()}
       </Grid>
-      <Grid item xs={4}>
-        <Button variant="contained" sx={{m:1}}>팀 생성</Button>
-      </Grid>
-      {CreateButton}
+      {createTeamButton()}
+      {manageTeamButtons}
     </Grid>
   );
 }
