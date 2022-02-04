dbf2bfa3aa8e5a0b11822a43ba0d1394cf64664e
dannyp0930 <dannyp0930@gmail.com>
2022-02-04 10:04:30 +0900

    REFACTOR: 팀 생성 모달창 수정중

--- frontend/src/components/team/CreateTeam.js
+++ frontend/src/components/team/CreateTeam.js
@@ -1,5 +1,18 @@
 import React from 'react';
-import { Grid, Button, Modal, Card } from '@mui/material'
+import {
+  Grid,
+  Button,
+  Modal,
+  Card,
+  TextField,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper,
+  Checkbox
+} from '@mui/material'
 
 function CreateTeam() {
   const modalStyle = {
@@ -15,6 +28,7 @@ function CreateTeam() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const userList = ['dannyp0930@gmail.com', 'dannyp0930@daum.net', 'dannyp95@naver.com']
 
   return (
     <Grid item>
@@ -24,7 +38,36 @@ function CreateTeam() {
         onClose={handleClose}
         >
         <Card sx={modalStyle}>
-          팀 생성
+          <h3>팀 생성</h3>
+          <hr></hr>
+          <TextField label="이름" variant="outlined" />
+          <br></br>
+          <br></br>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 300 }} aria-label="simple table">
+              <TableBody>
+                {userList.map((user) => (
+                  <TableRow
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell>{user}</TableCell>
+                    <TableCell padding="checkbox">
+                      <Checkbox
+                        color="primary"
+                        // checked={isItemSelected}
+                        // inputProps={{
+                        //   'aria-labelledby': labelId,
+                        // }}
+                      />
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <br></br>
+          <br></br>
+          <Button>생성</Button>
         </Card>
       </Modal>
     </Grid>
