4e901fe026aeda1b0f51ba57519aa0252b1ea62d
lactea94 <quddhs4694@naver.com>
2022-02-04 11:42:51 +0900

    FEAT: 회의창 분할

--- frontend/src/meetingrooms/Meetingroom.js
+++ frontend/src/meetingrooms/Meetingroom.js
@@ -1,9 +1,9 @@
 import React from 'react';
 // import Carousel from 'react-bootstrap/Carousel';
 import { styled }  from '@mui/material/styles';
-import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
+import "./Meetingroom.css"
 
 const Item = styled(Paper) (({ theme }) => ({
   ...theme.typography.body2,
@@ -15,41 +15,39 @@ const Item = styled(Paper) (({ theme }) => ({
 
 export const Meetingroom = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container direction="row" Spacing={1}>
-        <Grid container item xs={3} direction="column" justifyContent="center" alignItems="center">
-          <Grid item>
-            <Item>
-              모자걸이
-            </Item>
-          </Grid>
-          <Grid item>
-            <Item>
-              상세정보
-            </Item>
-          </Grid>
+    <Grid direction={"row"} container className='meetingbox'>
+      <Grid item container xs={2} direction="column" justifyContent="center" alignItems="center">
+        <Grid item>
+          <Item>
+            모자걸이
+          </Item>
         </Grid>
-        <Grid container item xs={3} spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          {Array.from(Array(6)).map((_, index) => (
-            <Grid item xs={2} sm={4} md={4} key={index}>
-              <Item>화면</Item>
-            </Grid>
-          ))}
-        </Grid>
-        <Grid container item xs={3} direction="column" justifyContent="center" alignItems="center">
-          <Grid item>
-            <Item>
-              회의 목표
-            </Item>
-          </Grid>
-          <Grid item>
-            <Item>
-              아이디어보드
-            </Item>
-          </Grid>
+        <Grid Item>
+          <Item>
+            상세정보
+          </Item>
         </Grid>
       </Grid>
-    </Box>
+      <Grid item container xs={7} spacing={10}>
+        {Array.from(Array(6)).map((_, index) => (
+          <Grid item xs={2} sm={4} md={4} key={index} className="facegrid">
+            <Item className="face">화면</Item>
+          </Grid>
+        ))}
+      </Grid>
+      <Grid item container xs={3} direction="column" justifyContent="center" alignItems="center">
+        <Grid item>
+          <Item>
+            회의 목표
+          </Item>
+        </Grid>
+        <Grid item>
+          <Item>
+            아이디어보드
+          </Item>
+        </Grid>
+      </Grid>
+    </Grid>
     );
 };
 
