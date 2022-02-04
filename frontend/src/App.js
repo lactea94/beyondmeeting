a09743c1bf3d32a95e399daf1977c7019c0cd4b2
dannyp0930 <dannyp0930@gmail.com>
2022-02-04 11:18:05 +0900

    FIX: 오타 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { Navbar } from './components/Navbar';
 import { Home } from './components/Home'
 import { Profile } from './components/Profile/Profile';
