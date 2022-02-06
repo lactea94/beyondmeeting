9571ec3bdf9d87cf4a8b1b1ec75a0e298e2bee97
dannyp0930 <dannyp0930@gmail.com>
2022-02-07 00:51:26 +0900

    REFACTOR: 모달 컴포넌트 분리

--- frontend/src/components/team/modal/CreateTeam.js
+++ frontend/src/components/team/modal/CreateTeam.js
@@ -10,20 +10,11 @@ import {
   TableContainer,
   TableRow,
   Paper,
+  Card,
 } from '@mui/material'
+import ModalStyle from './ModalStyle';
 
 function CreateTeam() {
-  const modalStyle = {
-    position: 'absolute',
-    width: '50%',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    bgcolor: 'background.paper',
-    boxShadow: 24,
-    p: 4,
-  };
-
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -41,62 +32,64 @@ function CreateTeam() {
         open={open}
         onClose={handleClose}
         >
-        <Grid container direction="column" rowSpacing={2} sx={modalStyle}>
-          <Grid item>팀 생성</Grid>
-          <Grid item>
-            <TextField label="이름" size="small" variant="outlined" />
-          </Grid>
-          <Grid item container
-            component="form"
-            onSubmit={function (event) {
-              event.preventDefault()
-              setUserList([...userList, { id: nextId.current, email: event.target.email.value}])
-              nextId.current += 1
-              event.target.email.value = ''
-            }}
-          >
+        <Card sx={ModalStyle()}>
+          <Grid container direction="column" rowSpacing={2}>
+            <Grid item>팀 생성</Grid>
             <Grid item>
-              <TextField
-                label="이메일"
-                name="email"
-                size="small"
-                variant="outlined"
-              />
+              <TextField label="이름" size="small" variant="outlined" />
+            </Grid>
+            <Grid item container
+              component="form"
+              onSubmit={function (event) {
+                event.preventDefault()
+                setUserList([...userList, { id: nextId.current, email: event.target.email.value}])
+                nextId.current += 1
+                event.target.email.value = ''
+              }}
+            >
+              <Grid item>
+                <TextField
+                  label="이메일"
+                  name="email"
+                  size="small"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  type="submit"
+                >
+                  추가
+                </Button>
+              </Grid>
             </Grid>
             <Grid item>
-              <Button
-                variant="contained"
-                type="submit"
-              >
-                추가
-              </Button>
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 300 }} size="small">
+                  <TableBody>
+                    {userList.map((user) => (
+                      <TableRow
+                        key={user.id}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                      >
+                        <TableCell>{user.email}</TableCell>
+                        <TableCell>
+                          <Button
+                            onClick={() => onRemove(user.id)}
+                          >삭제</Button>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Grid>
+            <Grid item>
+              <Button variant="contained">생성</Button>
             </Grid>
           </Grid>
-          <Grid item>
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 300 }} size="small">
-                <TableBody>
-                  {userList.map((user) => (
-                    <TableRow
-                      key={user.id}
-                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                      <TableCell>{user.email}</TableCell>
-                      <TableCell>
-                        <Button
-                          onClick={() => onRemove(user.id)}
-                        >삭제</Button>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Grid>
-          <Grid item>
-            <Button variant="contained">생성</Button>
-          </Grid>
-        </Grid>
+        </Card>
       </Modal>
     </Grid>
   );
