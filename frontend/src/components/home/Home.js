194ea6012c9c5cc84b5f4bf59db13d77c53b4ebf
lactea94 <quddhs4694@naver.com>
2022-02-08 18:14:19 +0900

    FIX: homepage 수정, favicon 입력, logo 삽입

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,7 +1,16 @@
+import './Home.css';
+import { Grid } from '@mui/material';
+
 export function Home () {
+
   return (
   <div>
-    <h1> Home page</h1>
+    <Grid className="mainLogoGrid">
+      <img className="mainLogoImage" src={require("./img/검정로고2.png")} alt="logoimage"></img>
+    </Grid>
+    <Grid>
+      <h2 style={{ color: 'white' }}>지금부터 회의를 시작합니다.</h2>
+    </Grid>
     <div className="img_">
       <img src={require("./img/그림1.png")} alt="karina"></img>
     </div>
