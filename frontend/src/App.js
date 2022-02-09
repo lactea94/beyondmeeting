4e12cd7774876b23ed8382d9cf14ca91357ea5c5
dannyp0930 <dannyp0930@gmail.com>
2022-02-09 15:43:52 +0900

    FEAT: 로그인 토큰 저장

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import { TeamList } from './components/team/TeamList';
 import { DefaultMeetingList } from './components/team/DefaultMeetingList';
 import { MeetingList } from './components/team/MeetingList';
 import { NotFound } from './common/NotFound';
+import { OAuth2RedirectHandler } from './users/OAuth2RedirectHandler';
 // import { useState } from 'react'
 
 function App() {
@@ -24,7 +25,7 @@ function App() {
           <Route path=':teamId' element={<MeetingList/>}></Route>
         </Route>
         <Route path='meetingroom' element={<Meetingroom />} />
-        <Route exact path='oauth2/redirect' element={<Home/>} />
+        <Route path='oauth2/redirect/*' element={<OAuth2RedirectHandler />} />
         <Route path='*' element={<NotFound/>}></Route>
       </Routes>
     </div>
