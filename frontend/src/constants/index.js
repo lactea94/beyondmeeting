26060c5391ffc7be68c536b74ed693d0790007e3
dannyp0930 <dannyp0930@gmail.com>
2022-02-09 15:47:50 +0900

    FEAT: 로그인 토큰 저장

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -8,9 +8,9 @@ export const RTC_BASE_URL = 'https://i6c101.p.ssafy.io:8443';
 export const ACCESS_TOKEN = 'accessToken';
 
 // 로컬 테스트
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
+// export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
 // 배포용
-// export const OAUTH2_REDIRECT_URI = 'https://i6c101.p.ssafy.io/oauth2/redirect'
+export const OAUTH2_REDIRECT_URI = 'https://i6c101.p.ssafy.io/oauth2/redirect'
 
 // 구글 로그인 URL
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
