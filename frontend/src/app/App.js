6245c23bed49917153e1770c148a36c5b6eb4881
dannyp0930 <dannyp0930@gmail.com>
2022-02-10 16:31:14 +0900

    REFACTOR: 앱 구조 변경

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import { Routes, Route, useLocation } from 'react-router-dom'
-import { Navbar } from './common/Navbar';
-import { Home } from './components/home/Home'
-import { Profile } from './components/profile/Profile';
-import { Meetingroom } from './meetingrooms/Meetingroom';
-import { TeamList } from './components/team/TeamList';
-import { DefaultMeetingList } from './components/team/DefaultMeetingList';
-import { MeetingList } from './components/team/MeetingList';
-import { NotFound } from './common/NotFound';
-import { OAuth2RedirectHandler } from './users/OAuth2RedirectHandler';
+import { Navbar } from '../common/Navbar';
+import { Home } from '../components/home/Home'
+import { Profile } from '../components/profile/Profile';
+import { Meetingroom } from '../components/meetingrooms/Meetingroom';
+import { TeamList } from '../components/team/TeamList';
+import { DefaultMeetingList } from '../components/team/DefaultMeetingList';
+import { MeetingList } from '../components/team/MeetingList';
+import { NotFound } from '../common/NotFound';
+import { OAuth2RedirectHandler } from '../users/OAuth2RedirectHandler';
 import { useState } from 'react'
-import { ACCESS_TOKEN } from './constants';
+import { ACCESS_TOKEN } from '../constants';
 
 function App() {
   const location = useLocation();
