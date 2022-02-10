97fd1fba1449c7b7331b9299caccd63330eb9395
pjyc17 <pjyc17@gmail.com>
2022-02-10 15:13:13 +0900

    FIX: APIUtils

--- frontend/src/util/APIUtils.js
+++ frontend/src/util/APIUtils.js
@@ -33,19 +33,3 @@ export function getCurrentUser() {
         method: 'GET'
     });
 }
-
-export function login(loginRequest) {
-    return request({
-        url: API_BASE_URL + "/auth/login",
-        method: 'POST',
-        body: JSON.stringify(loginRequest)
-    });
-}
-
-export function signup(signupRequest) {
-    return request({
-        url: API_BASE_URL + "/auth/signup",
-        method: 'POST',
-        body: JSON.stringify(signupRequest)
-    });
-}
