57ef159f9b01852d0386cf6fd5986250544061d7
lactea94 <quddhs4694@naver.com>
2022-02-11 19:10:40 +0900

    FIX:meetingroom editing

--- frontend/src/components/meetingrooms/Meetingroom.js
+++ frontend/src/components/meetingrooms/Meetingroom.js
@@ -1,45 +1,94 @@
 import React from 'react';
-import { Grid } from '@mui/material/'
+import "./Meetingroom.css"
+import { useState } from 'react';
+import { Grid, IconButton, Button } from '@mui/material/'
 import styled from 'styled-components'
+import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
+import { ReactComponent as RedHat } from './img/hat.svg'
 
 const Theme = styled.div`
-  text-align: "center";
   font-size: 18px;
-  height: 10%;
-
   @media only screen and (max-width: 900px) {
     font-size: 12px;
   }
 `
 
-const MainFunc = styled.div`
-  height: 80%;
-`
-
-const BottomBar = styled.div`
-  text-align: "center";
-  font-size: 18px;
-  height: 10%;
-
-  @media only screen and (max-width: 900px) {
-    font-size: 12px;
-  }
-`
+// const BottomBar = styled.div`
+//   font-size: 18px;
+//   @media only screen and (max-width: 900px) {
+//     font-size: 12px;
+//   }
+// `
 
 export const Meetingroom = () => {
-  return (
-    <Grid container>
-      <Theme item xs={12}>
-        회의 주제
-      </Theme>
+  const [openHatInfo, setOpenHatInfo] = useState(true);
+  const [openChatInfo, setOpenChatInfo] = useState(false);
+  const [openMemberInfo, setOpenMemberInfo] = useState(false);
 
-      <Grid item xs={12}>
-        <MainFunc>회의 주요 기능</MainFunc>
+
+
+  return (
+    <Grid className="room" container>
+      <Grid className="themeBox" item xs={12}>
+        <Theme className="theme">
+          회의 주제
+        </Theme>
       </Grid>
-      <Grid item xs={12}>
-        <BottomBar>
-          메뉴바
-        </BottomBar>
+      <Grid className="mainFuncBox" item xs={12}>
+        <div className="hatHanger">hat</div>
+        <div className="faceRoom">room</div>
+        <div className="chat">chat</div>
+        {/* <div className="mainFunc">회의 주요 기능</div> */}
+      </Grid>
+      <Grid className="bottomBarBox" container item xs={12}>
+        <Grid className="bottomBar" item xs={2}>
+          <Button 
+            variant="outlined" 
+            startIcon={<RedHat width="20"/>} 
+            onClick={() => {
+              setOpenHatInfo(!openHatInfo)
+              console.log("openHatInfo is", openHatInfo)
+              }}
+          >
+          모자
+          </Button>
+        </Grid>
+        <Grid item xs={1}></Grid>
+        <Grid className="bottomBar" item xs={2}>
+          음소거
+        </Grid>
+        <Grid className="bottomBar" item xs={2}>
+          화면공유
+        </Grid>
+        <Grid className="bottomBar" item xs={1}>
+          <IconButton>
+            <CancelRoundedIcon className="exitButton"></CancelRoundedIcon>
+          </IconButton>
+        </Grid>
+        <Grid item xs={1}></Grid>
+        <Grid 
+          className="bottomBar" 
+          item 
+          xs={1}
+          onClick={() => {
+            setOpenMemberInfo(!openMemberInfo)
+            console.log("openMemberInfo is", openMemberInfo)
+          }}
+        >
+          참여자
+        </Grid>
+        <Grid 
+          className="bottomBar" 
+          item 
+          xs={1}
+          onClick={() => {
+            setOpenChatInfo(!openChatInfo)
+            console.log("openChatInfo is", openChatInfo)
+          }}
+        >
+          채팅
+        </Grid>
+        <Grid item xs={1}></Grid>
       </Grid>
     </Grid>
     );
