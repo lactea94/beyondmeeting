4da6db906b6c4216cc73825f3d8dd8fc16627908
lactea94 <quddhs4694@naver.com>
2022-02-15 16:25:23 +0900

    FIX:delete temporary buttons and add register function in Meetinglist

--- frontend/src/components/meetingrooms/mainfunction/Videoroom.js
+++ frontend/src/components/meetingrooms/mainfunction/Videoroom.js
@@ -14,12 +14,8 @@ import
     sendMessage 
   } from './Kurento/conferenceroom.js';
 
-
-
-
 export default function Videoroom() {
 
-  console.log(window.location)
   return (
     <div id='participants' className="video-box">
 
