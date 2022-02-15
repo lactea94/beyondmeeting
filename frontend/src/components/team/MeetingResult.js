8e6d5676dde1886c4a3d24eef32924005a689426
dannyp0930 <dannyp0930@gmail.com>
2022-02-15 22:20:39 +0900

    REFACTOR: 미팅 결과 수정

--- frontend/src/components/team/MeetingResult.js
+++ frontend/src/components/team/MeetingResult.js
@@ -1,24 +1,31 @@
-import { Grid } from "@mui/material"
+import { Card, CardContent, CardHeader, Typography } from "@mui/material"
 import { useLocation } from "react-router-dom"
 
 export function MeetingResult() {
   const { state } = useLocation()
   const meeting = state.meeting
   return (
-    <div>
-      MeetingResult
-      <Grid>
-      {meeting.topic}
-      </Grid>
-      <Grid>
-      {meeting.meetingType}
-      </Grid>
-      <Grid>
-      {meeting.startTime}
-      </Grid>
-      <Grid>
-      {meeting.endTime}
-      </Grid>
-    </div>
+    <Card
+      sx={{
+        m: 'auto',
+        pb: 10,
+        height: '50rem',
+        width: '50%',
+      }}
+    >
+      <CardHeader title={meeting.topic}/>
+      <CardContent>
+        <Typography>
+          회의 종류 : 
+          {meeting.meetingType ? "육색모자" : "일반" } 
+        </Typography>
+        <Typography>
+          {meeting.startTime}
+        </Typography>
+        <Typography>
+          {meeting.endTime}
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
