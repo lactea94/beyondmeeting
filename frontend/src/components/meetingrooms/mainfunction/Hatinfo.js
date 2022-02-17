4fbeaf3ca2b912ec2827948280819a6e8c0a9454
lactea94 <quddhs4694@naver.com>
2022-02-18 03:28:54 +0900

    FIX:front/meeting

--- frontend/src/components/meetingrooms/mainfunction/Hatinfo.js
+++ frontend/src/components/meetingrooms/mainfunction/Hatinfo.js
@@ -4,34 +4,7 @@ import { ReactComponent as Hanger } from '../img/옷걸이.svg'
 
 
 export default function Hatinfo(props) {
-  // const [img, setImg] = useState('')
 
-  // useEffect(() => {
-  //   switch (props) {
-  //     case 'RED':
-  //       setImg('./img/모자2.png')
-  //       break; 
-  //     case 'GREEN':
-  //       setImg('./img/모자3.png')
-  //       break; 
-  //     case 'BLUE':
-  //       setImg('./img/모자4.png')
-  //       break; 
-  //     case 'WHITE':
-  //       setImg('./img/모자5.png')
-  //       break;
-  //     case 'BLACK':
-  //       setImg('./img/모자0.png')
-  //       break;
-  //     case 'YELLOW':
-  //       setImg('./img/모자1.png')
-  //       break;
-  //     default:
-  //       setImg(false)
-  //       break;
-  //   }
-  // }, [])
-  
   return (
     <div className="hat-info">
       <Hanger className='hanger-image' width='70%'></Hanger>
