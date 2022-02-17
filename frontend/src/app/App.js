42603ff0c17fd9f3cac79ff29c66c905a0c64f2d
dannyp0930 <dannyp0930@gmail.com>
2022-02-17 12:31:20 +0900

    FEAT: Footer 추가

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -57,6 +57,16 @@ export default function App() {
         <Route path='oauth2/redirect/*' element={<OAuth2RedirectHandler />} />
         <Route path='*' element={<NotFound/>}></Route>
       </Routes>
+      {location.pathname === '/' ? 
+      <div>
+        <hr className='footer-divide'></hr>
+        <div className='footer'>
+          <div className='footer-title'>Beyond Meeting</div>
+          <div className='footer-position'>광주광역시 광산구 하남산단6번로 107, SSAFY 2층</div>
+          <div className='footer-contents'>© 2022 ToMakeMattersBetter. All Rights Reserved.</div>
+        </div>
+      </div> :
+      null }
     </div>
   );
 }
