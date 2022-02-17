b9c4d837d76f33f3ef315867a85baaf37a91cad5
lactea94 <quddhs4694@naver.com>
2022-02-18 04:19:35 +0900

    FIX:hatinfo editing

--- frontend/src/components/meetingrooms/mainfunction/Hatinfo.js
+++ frontend/src/components/meetingrooms/mainfunction/Hatinfo.js
@@ -9,9 +9,8 @@ export default function Hatinfo(props) {
     <div className="hat-info">
       <Hanger className='hanger-image' width='70%'></Hanger>
       {props !== "NORMAL" ? <div className='hat-box'><img className='hat' src={require('./img/' + props + '.png')} alt='모자'/></div> : null}
-      <div className='info-box'>
-        
-      </div>
+      {/* {props !== "NORMAL" ? <div className='info-box'><img className='hat-info' src={require('./img/' + props + 'info.png')} alt='info'></img></div> : null} */}
+      <div className='info-box'><img className='hat-info' src={require('./img/REDinfo.png')} alt='info'></img></div>
     </div>
   );
 
